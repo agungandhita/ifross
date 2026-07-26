d6310a2dc9f705fac62ff135dd671b4a1360320d
agungandhita <agungandhita@gmail.com>
2026-07-27 03:00:05 +0700

    refactor: force HTTPS scheme on non-local environments instead of production only

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        if (! app()->isLocal()) {
             URL::forceScheme('https');
         }
 
